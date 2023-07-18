562ce04f1a57d5c1d28fcc6de26cd3f324be87dc
wooooooongs <pires11233@gmail.com>
2023-07-18 21:03:50 +0900

    :sparkles: feat(utils): 금일 버스 시간표 불러오는 함수 구현
    - fetchTodayBusTime: 현재 요일에 맞는 return

---
+++ whenMyBusGo/Utils/DateUtils.swift
@@ -0,0 +1,33 @@
+//
+//  DateUtils.swift
+//  whenMyBusGo
+//
+//  Created by 이재웅 on 2023/07/18.
+//
+
+import Foundation
+
+class DateUtils {
+    /// - Returns: ["00:00", "01:00", ...]
+    static func fetchTodayBusTime() -> [String] {
+        let date = Date()
+        let formatter = DateFormatter()
+        var currentWeek = ""
+        
+        formatter.dateFormat = "E"
+        formatter.locale = Locale(identifier: "ko_kr")
+        formatter.timeZone = TimeZone(abbreviation: "KST")
+        
+        currentWeek = formatter.string(from: date)
+        
+        switch currentWeek {
+        case "토":
+            return BusTimetable.saturday
+        case "일":
+            return BusTimetable.sunday
+        default:
+            return BusTimetable.weekday
+        }
+    }
+    
+}
