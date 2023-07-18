9334621d2a60fa7ad55a24a4dfc863e76a23b623
wooooooongs <pires11233@gmail.com>
2023-07-18 20:57:07 +0900

    :recycle: refactor(utils): static method로 변경 및 명칭 변경
    - 인스턴스 메서드에서 정적 메서드로 변경
    - parser -> converter로 명칭 변경

--- whenMyBusGo/Utils/TimeConverter.swift
+++ whenMyBusGo/Utils/TimeConverter.swift
@@ -7,16 +7,16 @@
 
 import Foundation
 
-class TimeParser {
-    var parsedTime = 0
-    
-    func parseTime(time: String) -> Int {
+class TimeConverter {
+    static func convertTimeToSeconds(time: String) -> Int {
+        var convertedTime = 0
+        
         let splitedTime = time.components(separatedBy: ":")
         
         if let hour = Int(splitedTime[0]), let minute = Int(splitedTime[1]) {
-            parsedTime = hour * 60 + minute
+            convertedTime = hour * 60 + minute
         }
         
-        return parsedTime
+        return convertedTime
     }
 }
