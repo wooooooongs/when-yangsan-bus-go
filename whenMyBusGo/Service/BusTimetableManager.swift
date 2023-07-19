4b4187532eebb15c7a1547ab84af98b8f18a5eea
wooooooongs <pires11233@gmail.com>
2023-07-19 18:02:48 +0900

    :recycle: refactor(service): swich문 -> subscript
    - 이를 위해 상수 데이터도 딕셔너리 타입에서 딕셔너리를 담은 딕셔너리로 변경

--- whenMyBusGo/Service/BusTimetableManager.swift
+++ whenMyBusGo/Service/BusTimetableManager.swift
@@ -11,18 +11,10 @@ class BusTimetableManager {
     /// - Returns: ["00:00", "01:00", ...]
     static func getTodayBusTime(busNum: Int) -> [String] {
         let currentWeek = DateUtils.getCurrentWeek()
-        var currentBusTimetable: [String: [String]] = [:]
+        let busNumKey = "bus\(busNum)"
+        var currentBusTimetable: [String: [String]]
         
-        switch busNum {
-        case 8:
-            currentBusTimetable = BusTimetable.bus8
-        case 1100:
-            currentBusTimetable = BusTimetable.bus1100
-        case 1200:
-            currentBusTimetable = BusTimetable.bus1200
-        default:
-            currentBusTimetable = [:]
-        }
+        currentBusTimetable = BusTimetable.timetable[busNumKey] ?? [:]
         
         if busNum == 8 {
             switch currentWeek {
