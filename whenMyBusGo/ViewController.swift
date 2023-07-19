4be151afb8087d838db8e86844554e4cf6663df8
wooooooongs <pires11233@gmail.com>
2023-07-19 23:02:21 +0900

    :sparkles: feat(view): 버스 데이터 불러오기 및 남은 시간 구현
    - 아직까진 8번 버스만 담당할 수 있음
    - 현재 시간 기준 버스 시간표 데이터에 다음 시간이 없으면 "막차 끊김"으로 출력
    - 30초마다 재출력

--- whenMyBusGo/ViewController.swift
+++ whenMyBusGo/ViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentBusDatas: [String] = []
+    var convertedBusDatas: [Int] = []
+    var nextBusIndex = 0
+    @IBOutlet weak var bus8TimeLeftLabel: UILabel!
+    @IBOutlet weak var bus8NextTimeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // 
+        
+        currentBusDatas = BusTimetableManager.getTodayBusTime(busNum: 8)
+        convertedBusDatas = convertBusDatas()
+        getNextBusInfo()
+        
+        Timer.scheduledTimer(timeInterval: 30.0, target: self, selector: #selector(getNextBusInfo), userInfo: nil, repeats: true)
+    }
+    
+    func convertBusDatas() -> [Int] {
+        return currentBusDatas.map(TimeConverter.convertTimeToSeconds)
+    }
+    
+    @objc func getNextBusInfo() {
+        let currentTime = DateUtils.getCurrentTime()
+        let convertedTime = TimeConverter.convertTimeToSeconds(time: currentTime)
+        
+        guard let nextBusIndex = convertedBusDatas.firstIndex(where: {$0 > convertedTime}) else {
+            bus8TimeLeftLabel.text = "막차 끊김"
+            bus8NextTimeLabel.text = currentBusDatas[0]
+            return
+        }
+        
+        let nextBusTime = currentBusDatas[nextBusIndex]
+        bus8NextTimeLabel.text = nextBusTime
+        
+        let minutesLeft = convertedBusDatas[nextBusIndex] - convertedTime
+        bus8TimeLeftLabel.text = "\(minutesLeft)분 남음"
     }
 
 
