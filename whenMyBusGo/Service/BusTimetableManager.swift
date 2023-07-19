65c3468d8fa80d2628c4e2743cdebac50f7a7904
wooooooongs <pires11233@gmail.com>
2023-07-19 17:54:13 +0900

    :recycle: refactor(service): 다양한 버스 관리를 위한 리팩토링
    기존에는 8번 버스만 관리했지만, 확장성을 위해 리팩토링

--- whenMyBusGo/Service/BusTimetableManager.swift
+++ whenMyBusGo/Service/BusTimetableManager.swift
@@ -9,16 +9,39 @@ import Foundation
 
 class BusTimetableManager {
     /// - Returns: ["00:00", "01:00", ...]
-    static func getTodayBusTime() -> [String] {
-        var currentWeek = DateUtils.getCurrentWeek()
+    static func getTodayBusTime(busNum: Int) -> [String] {
+        let currentWeek = DateUtils.getCurrentWeek()
+        var currentBusTimetable: [String: [String]] = [:]
         
-        switch currentWeek {
-        case "토":
-            return BusTimetable.saturday
-        case "일":
-            return BusTimetable.sunday
+        switch busNum {
+        case 8:
+            currentBusTimetable = BusTimetable.bus8
+        case 1100:
+            currentBusTimetable = BusTimetable.bus1100
+        case 1200:
+            currentBusTimetable = BusTimetable.bus1200
         default:
-            return BusTimetable.weekday
+            currentBusTimetable = [:]
+        }
+        
+        if busNum == 8 {
+            switch currentWeek {
+            case "토":
+                return currentBusTimetable["saturday"] ?? []
+            case "일":
+                return currentBusTimetable["sunday"] ?? []
+            default:
+                return currentBusTimetable["weekday"] ?? []
+            }
+        } else {
+            switch currentWeek {
+            case "토":
+                return currentBusTimetable["weekend"] ?? []
+            case "일":
+                return currentBusTimetable["weekend"] ?? []
+            default:
+                return currentBusTimetable["weekday"] ?? []
+            }
         }
     }
 }
