d95d3d02246aaba7908663bd4de780140d71d4b4
wooooooongs <pires11233@gmail.com>
2023-11-11 03:05:51 +0900

    :wrench: chore: rootViewController 탭바로 변경

--- whenMyBusGo/Resources/SceneDelegate.swift
+++ whenMyBusGo/Resources/SceneDelegate.swift
@@ -13,9 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-            
+        
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = BusTimetableViewController()
+        window.rootViewController = TabBarViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
