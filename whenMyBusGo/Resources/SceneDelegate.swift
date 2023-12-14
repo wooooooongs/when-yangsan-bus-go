5b45a3695d850375f75574046794597532b34b87
wooooooongs <pires11233@gmail.com>
2023-12-15 01:23:56 +0900

    :lipstick: style: window 기본 배경색 흰색 추가
    기본 색은 투명/검정임.

--- whenMyBusGo/Resources/SceneDelegate.swift
+++ whenMyBusGo/Resources/SceneDelegate.swift
@@ -17,6 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = TabBarViewController()
         window.makeKeyAndVisible()
+        window.backgroundColor = .white
         self.window = window
     }
 }
