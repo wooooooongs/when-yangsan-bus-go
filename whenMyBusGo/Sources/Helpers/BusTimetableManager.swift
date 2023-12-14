e99ba0a9902a2de531b8f04c58fc4169f0fc38c8
wooooooongs <pires11233@gmail.com>
2023-12-15 01:24:47 +0900

    :sparkles: feat: 모든 버스 데이터를 가져오는 getData 함수 구현
    busDatas.json 파일을 읽은 뒤 리턴한다

--- whenMyBusGo/Sources/Helpers/BusTimetableManager.swift
+++ whenMyBusGo/Sources/Helpers/BusTimetableManager.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class BusTimetableManager {
+    func getData() -> [BusTimetable] {
+        guard let jsonPath = Bundle.main.path(forResource: "busDatas", ofType: "json") else {
+            return []
+        }
+        let decoder = JSONDecoder()
+        
+        do {
+            let url = URL(filePath: jsonPath)
+            let data = try Data(contentsOf: url)
+            
+            let decodedData = try decoder.decode(BusTimetables.self, from: data)
+            
+            return decodedData.result
+        } catch {
+            print("busDatas.json이 없는 거 같은데용")
+            return []
+        }
+    }
     
 }
