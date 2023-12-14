693adea8030ce2954735ea187d448478f443a26c
wooooooongs <pires11233@gmail.com>
2023-12-15 01:27:16 +0900

    :sparkles: feat(Models): 버스 시간표 상세 모델 추가
    BusTimetables 구조체는 BusTimetable들을 result에 담는다.

--- whenMyBusGo/Sources/Models/BusTimetable.swift
+++ whenMyBusGo/Sources/Models/BusTimetable.swift
@@ -7,12 +7,60 @@
 
 import Foundation
 
-struct BusTimetables {
-    var busNumber: String = ""
-    var timetable: [BusTimetable] = []
+struct BusTimetables: Decodable {
+    let result: [BusTimetable]
 }
 
-struct BusTimetable {
-    var departure: String = ""
-    var buses: [String: [String]] = [:]
+struct BusTimetable: Decodable {
+    let busNumber: String
+    let id: String
+    let upbound: String
+    let downbound: String
+    private let type: String
+    var upboundTimetable: [Day: [String]] = [:]
+    var downboundTimetable: [Day: [String]] = [:]
+    
+    var busType: BusType {
+        return BusType(rawValue: type) ?? .일반
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case busNumber, id, upbound, downbound, type, upboundTimetable, downboundTimetable
+    }
+    
+    enum BusType: String {
+        case 일반 = "1"
+        case 좌석 = "2"
+        case 심야 = "3"
+        case 급행 = "4"
+        case 마을 = "5"
+    }
+}
+
+extension BusTimetable.BusType {
+    static func caseName(for rawValue: String) -> String? {
+        return BusTimetable.BusType(rawValue: rawValue)?.caseName
+    }
+    
+    var caseName: String {
+            switch self {
+            case .일반:
+                return "일반"
+            case .좌석:
+                return "좌석"
+            case .심야:
+                return "심야"
+            case .급행:
+                return "급행"
+            case .마을:
+                return "마을"
+            }
+        }
+}
+
+enum Day: String, Decodable{
+    case weekday
+    case sat
+    case sun
+    case weekend
 }
