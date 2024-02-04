5a293a225c950ab4ba71267bb28e5880c0b0f6a7
wooooooongs <pires11233@gmail.com>
2024-02-04 17:13:05 +0900

    :sparkles: feat: 각 테두리별로 border를 추가하는 메서드 구현

---
+++ whenMyBusGo/Sources/Helpers/UIView+addBorder.swift
@@ -0,0 +1,46 @@
+//
+//  UIView+addBorder.swift
+//  whenMyBusGo
+//
+//  Created by Oscar on 1/6/24.
+//
+
+import UIKit
+
+extension UIView {
+    func addBorder(_ edges: [UIRectEdge], withColor color: UIColor, width: CGFloat) {
+        self.layoutIfNeeded()
+        
+        for edge in edges {
+            let border = CALayer()
+            
+            switch edge {
+            case .all:
+                self.layer.borderWidth = width
+                break
+                
+            case .top:
+                border.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: width)
+                break
+                
+            case .right:
+                border.frame = CGRect(x: self.frame.size.width - width, y: 0, width: width, height: self.frame.size.height)
+                break
+                
+            case .bottom:
+                border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: width)
+                break
+                
+            case .left:
+                border.frame = CGRect(x: 0, y: 0, width: width, height: self.frame.size.height)
+                break
+                
+            default:
+                break
+            }
+            
+            border.backgroundColor = color.cgColor
+            self.layer.addSublayer(border)
+        }
+    }
+}
