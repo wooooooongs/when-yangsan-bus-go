dda2773c1f574cbbf51aa32b96b2a24f3e364da0
wooooooongs <pires11233@gmail.com>
2024-02-19 16:25:07 +0900

    :sparkles: feat(Models): 홈의 즐겨찾기 버스를 위한 모델로 수정

--- whenMyBusGo/Sources/Models/BusTimetable.swift
+++ whenMyBusGo/Sources/Models/BusTimetable.swift
@@ -20,42 +20,41 @@ struct BusTimetable: Decodable {
     var upboundTimetable: [Day: [String]] = [:]
     var downboundTimetable: [Day: [String]] = [:]
     
+    enum CodingKeys: String, CodingKey {
+        case busNumber, id, upbound, downbound, type, upboundTimetable, downboundTimetable
+    }
+        
+    // MARK: - 외부에서 버스 타입 접근
     var busType: BusType {
         return BusType(rawValue: type) ?? .일반
     }
-    
-    private enum CodingKeys: String, CodingKey {
-        case busNumber, id, upbound, downbound, type, upboundTimetable, downboundTimetable
-    }
-    
-    enum BusType: String {
-        case 일반 = "1"
-        case 좌석 = "2"
-        case 심야 = "3"
-        case 급행 = "4"
-        case 마을 = "5"
-    }
 }
 
-extension BusTimetable.BusType {
+enum BusType: String {
+    case 일반 = "1"
+    case 좌석 = "2"
+    case 심야 = "3"
+    case 급행 = "4"
+    case 마을 = "5"
+    
     static func caseName(for rawValue: String) -> String? {
-        return BusTimetable.BusType(rawValue: rawValue)?.caseName
+        return BusType(rawValue: rawValue)?.caseName
     }
     
     var caseName: String {
-            switch self {
-            case .일반:
-                return "일반"
-            case .좌석:
-                return "좌석"
-            case .심야:
-                return "심야"
-            case .급행:
-                return "급행"
-            case .마을:
-                return "마을"
-            }
+        switch self {
+        case .일반:
+            return "일반"
+        case .좌석:
+            return "좌석"
+        case .심야:
+            return "심야"
+        case .급행:
+            return "급행"
+        case .마을:
+            return "마을"
         }
+    }
 }
 
 enum Day: String, Decodable{
@@ -79,5 +78,21 @@ enum DayTypeForButton: Int {
         case .weekend:
             return .weekend
         }
+    }
+}
+
+struct BusTimetableForHomeView {
+    var busNumber: String
+    var upbound: String?
+    var downbound: String?
+    var busType: BusType
+    var busTypeCaseName: String
+    
+    init(busNumber: String, upbound: String?, downbound: String?, busType: BusType) {
+        self.busNumber = busNumber
+        self.upbound = upbound
+        self.downbound = downbound
+        self.busType = busType
+        self.busTypeCaseName = BusType.caseName(for: busType.rawValue) ?? "일반"
     }
 }
