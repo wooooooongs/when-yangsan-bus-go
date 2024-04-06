cc48ba2fbdf4a676735ce654e403bccd399da4bd
wooooooongs <pires11233@gmail.com>
2024-04-06 20:24:59 +0900

    :sparkles: (BusTB): 버스 타입 카테고리 UI 추가

--- whenMyBusGo/Sources/Presentation/BusTimetableView.swift
+++ whenMyBusGo/Sources/Presentation/BusTimetableView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct BusTimetableView: View {
+    let busTimetableManager = BusTimetableManager.shared
+    
+    let setBackground = Color(HexColor.from("EEEEEE"))
+    let busTypes = BusType.allCases
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            setBackground
+                .ignoresSafeArea()
+            
+            VStack {
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(busTypes, id: \.self) { busType in
+                            Button(action: {
+                            }) {
+                                Text("\(busType)")
+                                    .font(.callout)
+                                    .fontWeight(.medium)
+                                    .foregroundColor(
+                                        .white
+                                    )
+                                    .padding(.vertical, 8)
+                                    .padding(.horizontal, 16)
+                                    .background(Capsule().fill(
+                                        .yangsan
+                                    ))
+                            }
+                        }
+                    }
+                }
+                
+                Divider()
+                
+                ScrollView(.vertical) {
+                }
+                .padding(.top, 30.0)
+                
+                Spacer()
+            }
+            .padding(.top, 100.0)
+        }
+        .navigationTitle("버스 목록")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
+    //    ContentView()
+    //        .tint(.black)
     BusTimetableView()
 }
