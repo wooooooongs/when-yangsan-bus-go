7022018d76167f0edf47ad41b64d0791f75700d5
wooooooongs <pires11233@gmail.com>
2024-04-06 21:06:58 +0900

    :sparkles: (BusTB): 버스 목록 UI 및 데이터 넣기 구현

--- whenMyBusGo/Sources/Presentation/BusTimetableView.swift
+++ whenMyBusGo/Sources/Presentation/BusTimetableView.swift
@@ -32,6 +32,7 @@ struct BusTimetableView: View {
                             Button(action: {
                                 currentBusType = busType
                                 currentBusList = busTimetableManager.getBusTimetables(forType: busType)
+                                // TODO: 현재: Manager에 filter를 매번 요청함. 프론트에서 다루는 건 어떨까?
                             }) {
                                 Text("\(busType)")
                                     .font(.callout)
@@ -52,16 +53,58 @@ struct BusTimetableView: View {
                 Divider()
                 
                 ScrollView(.vertical) {
+                    VStack(spacing: 12.5) {
+                        ForEach(currentBusList, id: \.id) { item in
+                            busItem(for: item)
+                        }
+                    }
                 }
-                .padding(.top, 30.0)
+                .padding(.top, 10.0)
+                .padding([.leading, .trailing], 25)
                 
-                Spacer()
             }
             .padding(.top, 100.0)
         }
         .navigationTitle("버스 목록")
         .navigationBarTitleDisplayMode(.inline)
     }
+    
+    @ViewBuilder
+    private func busItem(for bus: BusTimetable) -> some View {
+        ZStack {
+            Color.white
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 5.0) {
+                    Text("\(bus.busNumber)")
+                        .font(.title)
+                        .fontWeight(.medium)
+                    
+                    HStack {
+                        Text("\(bus.busType)")
+                        
+                        Group {
+                            Text("\(bus.upbound)")
+                            Image(systemName: "repeat")
+                            Text("\(bus.downbound)")
+                        }
+                        .lineLimit(1)
+                        .truncationMode(.tail)
+                    }
+                    .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Image(systemName: "chevron.forward")
+            }
+            .padding([.leading, .trailing], 15)
+            .padding([.top, .bottom], 15)
+        }
+        .frame(maxWidth: .infinity)
+    }
+    
 }
 
 #Preview {
