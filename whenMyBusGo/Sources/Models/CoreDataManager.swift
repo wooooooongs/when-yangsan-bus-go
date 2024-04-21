78b6d07a1750bdd8617c43e8da15a05a0c7c2b29
wooooooongs <pires11233@gmail.com>
2024-04-21 22:02:35 +0900

    :sparkles: (Home): Manager 구현 및 즐겨찾기 리스트 불러오기

--- whenMyBusGo/Sources/Models/CoreDataManager.swift
+++ whenMyBusGo/Sources/Models/CoreDataManager.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import SwiftUI
 
 final class CoreDataManager {
     static let shared = CoreDataManager()
@@ -102,6 +103,7 @@ final class CoreDataManager {
             do {
                 if let fetchedFavoritedBusList = try context.fetch(request) as? [FavoritedBus] {
                     favoritedBusArray = fetchedFavoritedBusList
+                    print("데이터 가져옴~! \(fetchedFavoritedBusList)\n")
                 }
             } catch {
                 print("즐겨찾기 된 버스를 가져오는 데 실패했습니다!")
@@ -167,3 +169,98 @@ final class CoreDataManager {
     }
 }
 
+// MARK: - SwiftUI용 CoreData
+final class FavoritedBusDataManager: ObservableObject {
+    // MARK: - Setting
+    let modelName: String = "whenMyBusGo"
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: self.modelName)
+        
+        container.loadPersistentStores { _, error in
+            if let error {
+                fatalError("Persistent Stores를 불러오는 데 실패했습니다!: \(error.localizedDescription)")
+            }
+        }
+        
+        return container
+    }()
+    
+    lazy var viewContext: NSManagedObjectContext = persistentContainer.viewContext
+    
+    func saveContext() {
+        guard viewContext.hasChanges else { return }
+        
+        do {
+            try viewContext.save()
+        } catch {
+            print("저장에 실패하였습니다:", error.localizedDescription)
+        }
+    }
+    
+    // MARK: - CREATE & UPDATE
+    func createOrUpdateFavoriteBus(for busData: BusTimetable, isUpbound: Bool) {
+        let request: NSFetchRequest<FavoritedBus> = FavoritedBus.fetchRequest()
+        request.predicate = NSPredicate(format: "busId == %@", busData.id)
+        
+        do {
+            let result = try viewContext.fetch(request),
+                isFavoritedBusDataExist = result.first !== nil
+            
+            if isFavoritedBusDataExist,
+               let favoritedBus = result.first {
+                update(for: favoritedBus, isUpbound)
+            } else {
+                create(for: busData, isUpbound)
+            }
+        } catch {
+            print(error)
+        }
+        
+        saveContext()
+    }
+    
+    func create(for busData: BusTimetable, _ isUpbound: Bool) {
+        let newFavoritedBus = FavoritedBus(context: viewContext)
+        newFavoritedBus.busNumber = busData.busNumber
+        newFavoritedBus.busId = busData.id
+        newFavoritedBus.busType = BusType.caseName(for: busData.busType.rawValue)
+        newFavoritedBus.upboundName = busData.upbound
+        newFavoritedBus.downboundName = busData.downbound
+        newFavoritedBus.isUpboundFavorited = isUpbound
+        newFavoritedBus.isDownboundFavorited = !isUpbound
+        newFavoritedBus.savedDate = .now
+        
+        print("\(busData.id)가 존재하지 않습니다. 새로 생성합니다.")
+    }
+    
+    func update(for favoritedBus: FavoritedBus, _ isUpbound: Bool) {
+        if isUpbound {
+            favoritedBus.isUpboundFavorited.toggle()
+        } else {
+            favoritedBus.isDownboundFavorited.toggle()
+        }
+        
+        let isNotFavoritedAnyDirection: Bool = favoritedBus.isUpboundFavorited == false && favoritedBus.isDownboundFavorited == false
+        
+        if isNotFavoritedAnyDirection {
+            delete(item: favoritedBus)
+            print("")
+        }
+    }
+    
+    // MARK: - Delete
+    func deleteAll(items: [FavoritedBus]) {
+        for item in items {
+            viewContext.delete(item)
+        }
+        
+        saveContext()
+    }
+    
+    func delete(item: FavoritedBus) {
+        viewContext.delete(item)
+        
+        saveContext()
+    }
+}
--- whenMyBusGo/Sources/Presentation/Home/Views/HomeFavoritedBusListView.swift
+++ whenMyBusGo/Sources/Presentation/Home/Views/HomeFavoritedBusListView.swift
@@ -8,34 +8,78 @@
 import SwiftUI
 
 struct HomeFavoritedBusListView: View {
+    @EnvironmentObject var favoritedBusDataManager: FavoritedBusDataManager
+    @FetchRequest(entity: FavoritedBus.entity(), sortDescriptors: [
+        NSSortDescriptor(key: "savedDate", ascending: true)
+    ]) private var favoritedBus: FetchedResults<FavoritedBus>
+    
     var body: some View {
+        VStack(spacing: 10) {
+            ForEach(favoritedBus, id: \.id) { bus in
+                favoritedBusRows(for: bus)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func favoritedBusRows(for busData: FavoritedBus) -> some View {
+        let isBothFavorited = busData.isUpboundFavorited && busData.isDownboundFavorited
+        let isUpboundFavorited = busData.isUpboundFavorited && !busData.isDownboundFavorited
+        let isDownboundFavorited = !busData.isUpboundFavorited && busData.isDownboundFavorited
         
+        if isBothFavorited {
+            favoritedBusItem(for: busData, direction: busData.upboundName ?? "상행")
+            favoritedBusItem(for: busData, direction: busData.downboundName ?? "하행")
+        }
+        
+        if isUpboundFavorited {
+            favoritedBusItem(for: busData, direction: busData.upboundName ?? "상행")
+        }
+        
+        if isDownboundFavorited {
+            favoritedBusItem(for: busData, direction: busData.downboundName ?? "하행")
+        }
+    }
+    
+    
+    @ViewBuilder
+    private func favoritedBusItem(for busData: FavoritedBus, direction: String) -> some View {
         ZStack {
             Color.white
                 .clipShape(RoundedRectangle(cornerRadius: 10))
             
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("좌석 북정발")
-                        .font(.caption)
-                        .offset(y: 2.5)
+            if let busType = busData.busType,
+               let busNumber = busData.busNumber {
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(busType)
+                                .font(.caption)
+                                .offset(y: 2.5)
+                            
+                            Text(direction)
+                                .font(.caption)
+                                .offset(y: 2.5)
+                        }
+                        
+                        Text(busNumber)
+                            .font(.largeTitle)
+                    }
                     
-                    Text("1200")
-                        .font(.largeTitle)
+                    Spacer()
+                    
+                    VStack(alignment: .trailing) {
+                        Text("막차 끊김")
+                            .font(.callout)
+                        Text("06:55 출발")
+                            .font(.callout)
+                    }
                 }
-                
-                Spacer()
-                
-                VStack(alignment: .trailing) {
-                    Text("막차 끊김")
-                        .font(.callout)
-                    Text("06:55 출발")
-                        .font(.callout)
-                }
+                .padding([.vertical], 10)
+                .padding([.horizontal], 15)
             }
-            .padding([.leading, .trailing], 20)
         }
-        .frame(maxWidth: .infinity, maxHeight: 70.0)
+        .frame(maxWidth: .infinity)
     }
 }
 
